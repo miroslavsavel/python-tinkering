7e0413f835766bd00e8cba01f6b98b61d5fe8e3a
Bytesabre <miroslav.savel@gmail.com>
2022-12-01 09:53:59 +0100

    list comprehension

--- generators.py
+++ generators.py
@@ -10,13 +10,17 @@
 # my_nums = square_numbers([1,2,3,4,5])
 # print(my_nums)
 
-def square_numbers(nums):
-    result = []
-    for i in nums:
-        yield (i*i)
 
-my_nums = square_numbers([1,2,3,4,5])
+# def square_numbers(nums):
+#     result = []
+#     for i in nums:
+#         yield (i*i)
 
+# my_nums = square_numbers([1,2,3,4,5])
+
+my_nums = [x*x for x in [1,2,3,4,5]]
+
+print(my_nums)
 for num in my_nums:
     print(num)
 
